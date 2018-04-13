2fed4fdd774f545d38a6f81849d6f9e38f3fb8dd
岳常 <957513516@qq.com>
2018-04-13 10:37:23 +0800

    新增：BlockingQueue测试生产者消费者

---
+++ src/main/java/com/yc/concurrency/queue/Consumer.java
@@ -0,0 +1,51 @@
+/*!
+ * Copyright(c) 2017 Yue Chang
+ * MIT Licensed
+ */
+package com.yc.concurrency.queue;
+
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Yue Chang
+ * @ClassName: Consumer
+ * @Description: 消费者线程
+ * @date 2018/1/25 16:26
+ */
+public class Consumer implements Runnable{
+
+    private BlockingQueue<String> queue;
+    private static final int DEFAULT_RANGE_FOR_SLEEP = 1000;
+
+    public Consumer(BlockingQueue<String> queue) {
+        this.queue = queue;
+    }
+
+    public void run() {
+
+        System.out.println("启动消费者线程！~");
+        Random random = new Random();
+        boolean isRunning = true;
+        try{
+            while(isRunning){
+                System.out.println("正从队列中获取数据...");
+                String data = queue.poll(2, TimeUnit.SECONDS);
+                if (null != data){
+                    System.out.println("拿到数据：" + data);
+                    Thread.sleep(random.nextInt(DEFAULT_RANGE_FOR_SLEEP));
+                } else {
+                    // 超过2秒还没有数据，认为所有生产线程都已经退出，自动退出消费线程
+                    isRunning = false;
+                }
+            }
+        }catch (InterruptedException e) {
+
+            e.printStackTrace();
+            Thread.currentThread().interrupt();
+        }finally {
+            System.out.println("消费者线程退出！~");
+        }
+    }
+}
---
+++ src/main/java/com/yc/concurrency/queue/Producer.java
@@ -0,0 +1,56 @@
+/*!
+ * Copyright(c) 2017 Yue Chang
+ * MIT Licensed
+ */
+package com.yc.concurrency.queue;
+
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author Yue Chang
+ * @ClassName: Producer
+ * @Description: 生产者线程
+ * @date 2018/1/25 16:00
+ */
+public class Producer implements Runnable{
+
+    private volatile boolean isRunning = true;
+    private BlockingQueue queue;
+    private static AtomicInteger count = new AtomicInteger();
+    private static final int DEFAULT_RANGE_FOR_SLEEP = 1000;
+
+    public Producer(BlockingQueue queue) {
+        this.queue = queue;
+    }
+
+    public void run() {
+        String data = null;
+        Random r = new Random();
+
+        System.out.println("生产者线程启动！~");
+        try{
+            while (isRunning){
+                System.out.println("正在生产数据...");
+                Thread.sleep(r.nextInt(DEFAULT_RANGE_FOR_SLEEP));
+
+                data = "data" + count.incrementAndGet();
+                System.out.println("将数据" + data + "放入队列！~");
+                if (queue.offer(data,2, TimeUnit.SECONDS)){
+                    System.out.println("放入数据失败：" + data);
+                }
+            }
+        }catch (InterruptedException e){
+            e.printStackTrace();
+            Thread.currentThread().interrupt();;
+        }finally {
+            System.out.println(Thread.currentThread().getName() + "生产者线程结束！~");
+        }
+    }
+
+    public void stop() {
+        isRunning = false;
+    }
+}
