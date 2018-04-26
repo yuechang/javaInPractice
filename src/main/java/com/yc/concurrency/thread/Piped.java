54dbc69ab618ab93110345ee5bd2839307565f73
岳常 <957513516@qq.com>
2018-04-26 10:16:45 +0800

    新增：管道输入/输出流示例

---
+++ src/main/java/com/yc/concurrency/thread/Piped.java
@@ -0,0 +1,66 @@
+/*!
+ * Copyright(c) 2017 Yue Chang
+ * MIT Licensed
+ */
+package com.yc.concurrency.thread;
+
+import java.io.IOException;
+import java.io.PipedReader;
+import java.io.PipedWriter;
+
+/**
+ * @author Yue Chang
+ * @ClassName: Piped
+ * @Description: 管道输入/输出流示例
+ * @date 2018/4/25 19:41
+ */
+public class Piped {
+
+    public static void main(String[] args) throws IOException {
+        PipedWriter out = new PipedWriter();
+        PipedReader in = new PipedReader();
+        // 将输出溜和输入流进行连接，否则在使用时会抛出IOException
+        out.connect(in);
+
+        Thread printThread = new Thread(new Print(in), "PrintThread");
+        printThread.start();
+
+        int receive = 0;
+        try {
+            while ((receive = System.in.read()) != -1){
+                out.write(receive);
+            }
+        } finally {
+            out.close();
+        }
+    }
+
+    static class Print implements Runnable{
+
+        private PipedReader in;
+
+        public Print(PipedReader in) {
+            this.in = in;
+        }
+
+        @Override
+        public void run() {
+            int receive = 0;
+            try {
+                while((receive = in.read()) != -1){
+                    System.out.print((char)receive);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+/*
+
+运行该示例，输入一组字符串，可以看到被printThread进行了原样输出。
+Repeat my words
+Repeat my words
+对于Piped类型的流，必须先要进行绑定，也就是调用connect()方法，如果没有将输入/输
+出流绑定起来，对于该流的访问将会抛出异常。
+ */
