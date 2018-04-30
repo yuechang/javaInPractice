d93884a750033e66a1b7873534811681013d247a
岳常 <957513516@qq.com>
2018-04-30 20:24:35 +0800

    新增：读写锁(ReentrantReadWriteLock)示例

---
+++ src/main/java/com/yc/concurrency/lock/Cache.java
@@ -0,0 +1,57 @@
+/*!
+ * Copyright(c) 2017 Yue Chang
+ * MIT Licensed
+ */
+package com.yc.concurrency.lock;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * @author Yue Chang
+ * @ClassName: Cache
+ * @Description: 读写锁示例
+ * @date 2018/4/30 20:10
+ */
+public class Cache {
+
+    static Map<String, Object> map = new HashMap<>();
+    static ReentrantReadWriteLock rw1 = new ReentrantReadWriteLock();
+    static Lock r = rw1.readLock();
+    static Lock w = rw1.writeLock();
+
+    // 获取一个key对应的value
+    public static final Object get(String key) {
+        r.lock();
+        try {
+            return map.get(key);
+        } finally {
+            r.unlock();
+        }
+    }
+    // 设置key对应的value
+    public static final Object put(String key, Object value) {
+        w.lock();
+        try {
+            return map.put(key, value);
+        } finally {
+            w.unlock();
+        }
+    }
+    // 清空所有的内容
+    public static final void clear() {
+        w.lock();
+        try {
+            map.clear();
+        } finally {
+            w.unlock();
+        }
+    }
+}
+/*
+
+
+
+ */
