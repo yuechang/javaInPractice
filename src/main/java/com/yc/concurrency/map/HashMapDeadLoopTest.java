2a0913608b19769235d17fc881a7670ee432be61
岳常 <957513516@qq.com>
2018-05-02 22:02:17 +0800

    新增：HashMap死循环示例示例(jdk8下未重现)

---
+++ src/main/java/com/yc/concurrency/map/HashMapDeadLoopTest.java
@@ -0,0 +1,45 @@
+/*!
+ * Copyright(c) 2017 Yue Chang
+ * MIT Licensed
+ */
+package com.yc.concurrency.map;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * @author Yue Chang
+ * @ClassName: HashMapDeadLoopTest
+ * @Description: HashMap死循环示例
+ * @date 2018/5/2 21:39
+ */
+public class HashMapDeadLoopTest {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        final Map<String, String> map = new HashMap<>(2);
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10000; i++) {
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            map.put(UUID.randomUUID().toString(), "");
+                        }
+                    }, "ftf" + i).start();
+                }
+            }
+        },"ftf");
+        t.start();
+        t.join();
+    }
+}
+/*
+环境信息：
+windows 10
+jdk1.8.0_101
+
+未发生死锁，哈哈哈
+ */
