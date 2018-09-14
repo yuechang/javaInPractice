21f5e0254e7ada48b57a32aa2497ed00929f276f
yuechang <957513516@qq.com>
2018-09-14 14:48:49 +0800

    新增：Arrays.asList()转化为集合后调用add/remove/clear方法测试

--- src/main/java/com/yc/base/collection/ListTest.java
+++ src/main/java/com/yc/base/collection/ListTest.java
@@ -1,6 +1,7 @@
 package com.yc.base.collection;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
@@ -10,14 +11,15 @@ import java.util.List;
  * @Description: List sublist测试
  * @date 2018/9/12 16:12
  */
-public class ListSubListTest {
+public class ListTest {
 
     public static int LIST_COUNT = 10;
 
     public static void main(String[] args) {
         testSubListToArrayList();
         testSubList();
-        testSubListToArray();
+        testListToArray();
+        testArrayAsList();
     }
 
     public static void testSubListToArrayList() {
@@ -61,7 +63,7 @@ public class ListSubListTest {
     }
 
 
-    public static void testSubListToArray() {
+    public static void testListToArray() {
 
         try {
 
@@ -81,6 +83,21 @@ public class ListSubListTest {
         }
     }
 
+
+    public static void testArrayAsList() {
+
+        try {
+
+            String[] arr = {"hello", ",", "world"};
+            List<String> list = Arrays.asList(arr);
+
+            list.add("!");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
 /*
 Java开发书册List集合规则：
@@ -92,7 +109,8 @@ Java开发书册List集合规则：
 4. 【强制】使用集合转数组的方法，必须使用集合的toArray(T[] array)，传入的是类型完全一样的数组，大小就是list.size()。
 说明：使用toArray带参方法，入参分配的数组空间不够大时，toArray方法内部将重新分配内存空间，并返回新数组地址；
 如果数组元素大于实际所需，下标为[ list.size() ]的数组元素将被置为null，其它数组元素保持原值，因此最好将方法入参数组大小定义与集合元素个数一致。
-
+5. 【强制】使用工具类Arrays.asList()把数组转换成集合时，不能使用其修改集合相关的方法，它的add/remove/clear方法会抛出UnsupportedOperationException异常。
+说明：asList的返回对象是一个Arrays内部类，并没有实现集合的修改方法。Arrays.asList体现的是适配器模式，只是转换接口，后台的数据仍是数组
 
 程序结果：
 java.lang.ClassCastException: java.util.ArrayList$SubList cannot be cast to java.util.ArrayList
@@ -107,5 +125,10 @@ java.util.ConcurrentModificationException
 	at com.yc.base.collection.ListSubListTest.main(ListSubListTest.java:19)
 arr:[Ljava.lang.String;@16b3fc9e
 tempArr:[Ljava.lang.Object;@e2d56bf
+java.lang.UnsupportedOperationException
+	at java.util.AbstractList.add(AbstractList.java:148)
+	at java.util.AbstractList.add(AbstractList.java:108)
+	at com.yc.base.collection.ListTest.testArrayAsList(ListTest.java:93)
+	at com.yc.base.collection.ListTest.main(ListTest.java:22)
 
  */
