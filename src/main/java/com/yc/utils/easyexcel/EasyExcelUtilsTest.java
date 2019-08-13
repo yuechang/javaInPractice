9551a938641701beec420ee74363f6fbbf53b266
yuechang <957513516@qq.com>
2019-08-14 00:03:54 +0800

    新增：easyexcel工具类

---
+++ src/main/java/com/yc/utils/easyexcel/EasyExcelUtilsTest.java
@@ -0,0 +1,60 @@
+/*!
+ * Copyright(c) 2017 Yue Chang
+ * MIT Licensed
+ */
+package com.yc.utils.easyexcel;
+
+import com.alibaba.excel.support.ExcelTypeEnum;
+import org.junit.Test;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Yue Chang
+ * @ClassName: EasyExcelUtilsTest
+ * @Description: 工具测试类
+ * @date 2019/8/13 23:51
+ */
+public class EasyExcelUtilsTest {
+
+    @Test
+    public void writeExcel() throws FileNotFoundException {
+        List<ExcelModel> excelModelList = new ArrayList<>();
+        for (int i = 0; i < 1000000; i++) {
+            ExcelModel excelModel = new ExcelModel();
+            excelModel.setAddress("address" + i);
+            excelModel.setAge(i + "");
+            excelModel.setEmail("email" + i);
+            excelModel.setHeigh("heigh" + i);
+            excelModel.setLast("last" + i);
+            excelModel.setName("name" + i);
+            excelModel.setSax("sax" + i);
+            excelModelList.add(excelModel);
+        }
+
+        long beginTime = System.currentTimeMillis();
+        OutputStream out = new FileOutputStream("D:/aaa.xlsx");
+        EasyExcelUtils.writeExcelWithModel(out, excelModelList, ExcelModel.class, ExcelTypeEnum.XLSX);
+        long endTime = System.currentTimeMillis();
+        System.out.println(String.format("总共耗时 %s 毫秒", (endTime - beginTime)));
+
+        excelModelList = null;
+    }
+
+    @Test
+    public void readExcel() throws FileNotFoundException {
+        try {
+            InputStream inputStream=new FileInputStream("D:/aaa.xlsx");
+            //读入文件，每一行对应一个 Model，获取 Model 列表
+            List<ExcelModel> objectList = EasyExcelUtils.readExcelWithModel(inputStream, ExcelModel.class, true);
+            for(ExcelModel excelModel: objectList) {
+                System.out.println(excelModel);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
---
+++ src/main/java/com/yc/utils/easyexcel/ExcelModel.java
@@ -0,0 +1,110 @@
+/*!
+ * Copyright(c) 2017 Yue Chang
+ * MIT Licensed
+ */
+package com.yc.utils.easyexcel;
+
+import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.metadata.BaseRowModel;
+
+import java.io.Serializable;
+
+/**
+ * @author Yue Chang
+ * @ClassName: ExcelModel
+ * @Description: Excel实体模型
+ * @date 2019/8/13 23:49
+ */
+public class ExcelModel extends BaseRowModel implements Serializable {
+
+    @ExcelProperty(value = "姓名" ,index = 0)
+    private String name;
+
+    @ExcelProperty(value = "年龄",index = 1)
+    private String age;
+
+    @ExcelProperty(value = "邮箱",index = 2)
+    private String email;
+
+    @ExcelProperty(value = "地址",index = 3)
+    private String address;
+
+    @ExcelProperty(value = "性别",index = 4)
+    private String sax;
+
+    @ExcelProperty(value = "高度",index = 5)
+    private String heigh;
+
+    @ExcelProperty(value = "备注",index = 6)
+    private String last;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getSax() {
+        return sax;
+    }
+
+    public void setSax(String sax) {
+        this.sax = sax;
+    }
+
+    public String getHeigh() {
+        return heigh;
+    }
+
+    public void setHeigh(String heigh) {
+        this.heigh = heigh;
+    }
+
+    public String getLast() {
+        return last;
+    }
+
+    public void setLast(String last) {
+        this.last = last;
+    }
+
+    @Override
+    public String toString() {
+        return "ExcelModel{" +
+                "name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                ", sax='" + sax + '\'' +
+                ", heigh='" + heigh + '\'' +
+                ", last='" + last + '\'' +
+                '}';
+    }
+}
+
