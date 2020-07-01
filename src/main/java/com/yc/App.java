6cdf82a90620967b2a527a0b3e124a7cc0db873c
yuechang <957513516@qq.com>
2020-07-01 21:26:37 +0800

    优化：修改代码格式

--- src/main/java/com/yc/App.java
+++ src/main/java/com/yc/App.java
@@ -4,8 +4,7 @@ package com.yc;
  * Hello world!
  */
 public class App {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
